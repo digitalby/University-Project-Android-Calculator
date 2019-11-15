5a28bf2a4b87c14ec0ad709c1ed25c121d64ef3c
digitalby <yury@digitalby.me>
2019-11-15 23:34:02 +0300

    Implement most basic functions & bug fixes

--- app/src/main/java/com/example/lr2/MainActivity.kt
+++ app/src/main/java/com/example/lr2/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.text.isDigitsOnly
 import androidx.databinding.DataBindingUtil
 import com.example.lr2.databinding.ActivityMainBinding
+import kotlin.math.absoluteValue
+import kotlin.math.roundToLong
 
 enum class CalculatorOperation {
     None,
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     var currentNumberString = ""
     var currentOperation: CalculatorOperation = CalculatorOperation.None
+    var firstNumber: Float? = null
     lateinit var binding: ActivityMainBinding
 
     fun setButtonActions() {
@@ -34,19 +37,109 @@ class MainActivity : AppCompatActivity() {
         binding.button9.setOnClickListener{ inputNumber("9") }
         binding.buttonDecimal.setOnClickListener{ inputNumber(".") }
         binding.buttonC.setOnClickListener{ inputNumber("C") }
+        binding.buttonPlus.setOnClickListener{ buttonOperation(CalculatorOperation.Add) }
+        binding.buttonMinus.setOnClickListener{ buttonOperation(CalculatorOperation.Subtract) }
+        binding.buttonMultiply.setOnClickListener{ buttonOperation(CalculatorOperation.Multiply) }
+        binding.buttonDivide.setOnClickListener{ buttonOperation(CalculatorOperation.Divide) }
+        binding.buttonEquals.setOnClickListener{ doCurrentOperation() }
+        binding.buttonPlusMinus.setOnClickListener{ doPlusMinus() }
+        binding.buttonPercent.setOnClickListener { doPercent() }
+    }
+    
+    //TODO instant operations
+    //TODO function for making string (both normal and short) with and without outputting
+
+    fun parseCurrentString(): Float {
+        if(currentNumberString.isNullOrEmpty() || currentNumberString == "0.") {
+            return 0f
+        }
+        return currentNumberString.toFloat()
     }
 
-    fun doOperation() {
+    fun toCurrentString(num: Float) {
+        var ret = num.toString()
+        //FIXME long strings and big numbers are not handled!
 
+        //TODO handle numbers that are too long (>=1 billion) or with too long decimal parts. don't forget MINUS!
+        //TODO handle both portrait and landscape (introduce a new variable?)
+        currentNumberString = ret
+        binding.mainTextView.text = currentNumberString
+    }
+
+    fun doPlusMinus() {
+        if(currentNumberString.isNullOrEmpty() || currentNumberString == "Error")
+            return
+        if(currentNumberString.contains("-")) {
+            currentNumberString = currentNumberString.replace("-","")
+        } else {
+            if(parseCurrentString() != 0f) {
+                currentNumberString = "-${currentNumberString}"
+            }
+        }
+        binding.mainTextView.text = currentNumberString
+    }
+
+    fun doPercent() {
+        if(currentNumberString == "Error")
+            return
+        var num = parseCurrentString()
+        num /= 100f
+        toCurrentString(num)
+    }
+
+    fun buttonOperation(operation: CalculatorOperation) {
+        if(currentNumberString == "Error")
+            return
+        if(!currentNumberString.isNullOrEmpty()) {
+            doCurrentOperation()
+            firstNumber = parseCurrentString()
+            currentNumberString = ""
+            binding.mainTextView.text = "0"
+        }
+        currentOperation = operation
+    }
+
+    fun doCurrentOperation() {
+        if(currentNumberString == "Error")
+            return
+        val secondNumber = parseCurrentString()
+        var result: Float = 0f
+        when(currentOperation) {
+            CalculatorOperation.None -> return
+            CalculatorOperation.Add -> {
+                result = firstNumber!! + secondNumber
+            }
+            CalculatorOperation.Subtract -> {
+                result = firstNumber!! - secondNumber
+            }
+            CalculatorOperation.Multiply -> {
+                result = firstNumber!! * secondNumber
+            }
+            CalculatorOperation.Divide -> {
+                if(secondNumber == 0f) {
+                    currentNumberString = "Error"
+                    binding.mainTextView.text = currentNumberString
+                } else {
+                    result = firstNumber!! / secondNumber
+                }
+            }
+        }
+        if(currentNumberString != "Error")
+            toCurrentString(result)
+        firstNumber = null
+        currentOperation = CalculatorOperation.None
     }
 
     fun inputNumber(symbol: String) {
         if(symbol == "C") {
+            currentOperation = CalculatorOperation.None
             currentNumberString = ""
             binding.mainTextView.text = "0"
             return
         }
-        if(currentNumberString.length == 8) {
+        if(currentNumberString == "Error")
+            return
+        if(currentNumberString.length == 8) { //TODO handle landscape and don't hardcode length
             return
         } else if(currentNumberString.isNullOrEmpty()) {
             if(symbol == ".") {
